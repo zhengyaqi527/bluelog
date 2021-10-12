dd7550b78741a5bcfc2f4639aa5e83f062acbbdb
zhengyaqi <670567234@qq.com>
2021-10-12 21:59:39 +0800

    add models and forms

---
+++ bluelog/models.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+
+from bluelog.extensions import db
+
+
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+    blog_title = db.Column(db.String(60))
+    blog_sub_title = db.Column(db.String(100))
+    name = db.Column(db.String(30))
+    about = db.Columnt(db.Text)
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+    posts = db.relationshp('Post', back_populates='category')
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship('Categorp', back_populates='posts')
+    comments = db.relationship('Comment', back_populates='post', cascase='all')
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(30))
+    email = db.Column(db.String(254))
+    site = db.Column(db.String(255))
+    body = db.Column(db.Text)
+    from_admin = db.Column(db.Boolean, default=False) # 是否是管理员的评论
+    reviewed = db.Column(db.Boolean, default=False)  # 是否通过审核
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    post = db.relationship('Post', back_populates='comments')
+    replied_id = db.Column(db.Intefer, db.ForeignKey())
